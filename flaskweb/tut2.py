0b12c6d602c3c5335dc546746d723e40d629eaed
Harsh Mangal <51486214+harshmangal1108@users.noreply.github.com>
2019-09-17 01:25:05 +0530

    flask web app

---
+++ flaskweb/tut2.py
@@ -0,0 +1,12 @@
+##### Serving image from static folder using jinja template
+from flask import Flask,render_template
+app=Flask(__name__)
+
+@app.route("/")
+def hello():
+	name ="Harsh Mangal"
+	return render_template("index.html" ,name1=name)
+@app.route("/harsh")
+def harsh():
+	return render_template("harsh.html")
+app.run(debug=True)
